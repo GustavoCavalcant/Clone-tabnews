cdf4ff873fb71572cac6d0aceabffc42d407d782
Gustavo Cavalcante Ferreira <gustavo2402.00@gmail.com>
2025-12-12 00:14:50 +0000

    Ajuste de texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <p>Gusta do futuro, pode ser que algum dia você veja isso, saiba que quando gerou esse simples texto, você estava bem frustrado em varios âmbitos da sua vida. Espero que tenha se encontrado e esteja mais realizado e menos ansioso.                          <b>Eu acredito muito em você!!!</b></p>
+    return <p>Vamo que vamo!!</p>
 }
 
 export default Home;
